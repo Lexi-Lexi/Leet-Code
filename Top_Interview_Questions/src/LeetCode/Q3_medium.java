ecf59c3ee95b2f65028e41bf082c59b8c1487e53
Lexi-Lexi <pauliemc@naver.com>
2022-08-30 23:22:02 +0200

    Code added

---
+++ Top_Interview_Questions/src/LeetCode/Q3_medium.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q3_medium {
+	/* Given a string s, find the length of the longest substring without repeating characters.
+	 * s consists of English letters, digits, symbols and spaces. */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Solution3.lengthOfLongestSubstring("abcabcbb")); // 3
+		System.out.println(Solution3.lengthOfLongestSubstring("bbbbb")); // 1
+		System.out.println(Solution3.lengthOfLongestSubstring("pwwkew")); // 3
+		
+		
+	}
+
+}
+
+
+class Solution3{
+	public static int lengthOfLongestSubstring(String s) {
+		int i = 0, j = 0, max = 0;
+	    Set<Character> set = new HashSet<>();
+	    
+	    while (j < s.length()) {
+	        if (!set.contains(s.charAt(j))) {
+	            set.add(s.charAt(j++));
+	            max = Math.max(max, set.size());
+	        } else {
+	            set.remove(s.charAt(i++));
+	        }
+	    }
+	    
+	    return max;
+	 
+    }
+}
