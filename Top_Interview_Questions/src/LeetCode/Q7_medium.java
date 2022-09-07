c897f5031e1de7154a8142ee27ddfeea68c563a8
Lexi-Lexi <pauliemc@naver.com>
2022-09-07 23:19:01 +0200

    Code fixed

--- Top_Interview_Questions/src/LeetCode/Q7_medium.java
+++ Top_Interview_Questions/src/LeetCode/Q7_medium.java
@@ -1,20 +1,18 @@
 package LeetCode;
 
-import java.util.Stack;
-
-public class Q6_medium {
+public class Q7_medium {
 	/* Given a signed 32-bit integer x, return x with its digits reversed.
 	 * If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
 	 * Assume the environment does not allow you to store 64-bit integers (signed or unsigned). 
 	 */
 
 	public static void main(String[] args) {
-		System.out.println(Solution6.reverse(-12340));
+		System.out.println(Solution7.reverse(-12340));
 	}
 
 }
 
-class Solution6 {
+class Solution7 {
 	public static int reverse(int x) {
 		
 		// I first tried using stack, but it exceeded time limit.
