826fc8c3a518dacca308b289c12e575c77e2c86f
Lexi-Lexi <pauliemc@naver.com>
2024-03-24 22:06:58 +0100

    A bubble sort

--- BookPractice/src/LeetCode/Book_04_2750.java
+++ BookPractice/src/LeetCode/Book_04_2750.java
@@ -21,7 +21,7 @@ public class Book_04_2750 {
 		
 		for (int i = 0; i < n-1; i++)
 		{
-			for (int j = 0; j < n-1; j++)
+			for (int j = 0; j < n-1-i; j++)
 			{
 				if (array[j] > array[j+1])
 				{
